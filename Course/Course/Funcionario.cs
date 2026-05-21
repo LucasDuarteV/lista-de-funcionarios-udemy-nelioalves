66b18952404c345e19aea8936a6b0b588a1fa165
LucasDuarteV <lucasdv2733@gmail.com>
2026-05-21 13:40:54 -0300

    subindo projeto

---
+++ Course/Course/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Course
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoDeSalario(double porcetagem)
+        {
+            Salario += Salario * porcetagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2" , CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+using System.Xml;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Funcionario> lista = new List<Funcionario>();
+
+            Console.Write("Quantas pessoas registradas: ");
+            int pessoasRegistradas = int.Parse(Console.ReadLine()!);
+            
+            for (int i = 0; i < pessoasRegistradas; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Funcionario: {i+1}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine()!);
+                Console.Write("Nome: ");
+                string nome =Console.ReadLine()!;
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine()! , CultureInfo.InvariantCulture);
+
+                Funcionario funcionario = new Funcionario(id , nome, salario);
+
+                lista.Add(funcionario);
+            }
+
+            Console.WriteLine();
+            Console.Write("Entre com id da pessoa para aumento: ");
+            int idFunc = int.Parse(Console.ReadLine()!);
+
+            Funcionario comparaId = lista.Find(x => x.Id == idFunc)!;
+
+            if (comparaId != null)
+            {
+                Console.WriteLine();
+                Console.Write("Digite a porcentagem: ");
+                double porcetagem = double.Parse(Console.ReadLine()! , CultureInfo.InvariantCulture);
+                comparaId.AumentoDeSalario(porcetagem);
+            }
+            else
+            {
+                Console.WriteLine("ID não correspondente.");
+            }
+
+            foreach (var listaFuncionario in lista)
+            {
+                Console.WriteLine();
+                Console.WriteLine(listaFuncionario);
+            }
+        }
+    }
+}
